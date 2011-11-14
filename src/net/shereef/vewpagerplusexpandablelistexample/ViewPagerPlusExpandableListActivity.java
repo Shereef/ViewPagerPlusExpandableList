b5af182acbdd7ad0a4f6f1720fab20d4696f600a
Shereef Marzouk <github@shereef.net>
2011-11-14 15:55:51 +0200

    Initial commit (Created Project).

---
+++ src/net/shereef/vewpagerplusexpandablelistexample/ViewPagerPlusExpandableListActivity.java
@@ -0,0 +1,13 @@
+package net.shereef.vewpagerplusexpandablelistexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewPagerPlusExpandableListActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
